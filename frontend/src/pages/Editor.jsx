606e1d84637072c7fb6db3ba3330e91b34bae037
Vinicios Leite de Campos <vinicios.ldcampos@gmail.com>
2026-06-04 16:41:05 -0300

    feat: adiciona Toolbar do editor com ferramentas, zoom e toggle 2D/3D

---
+++ frontend/src/pages/Editor.jsx
@@ -0,0 +1,9 @@
+import Toolbar from "../components/editor/Toolbar";
+
+export default function Editor() {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      <Toolbar />
+    </div>
+  );
+}
