2320be9b997021b4253b066c4264cf8822ea22ed
Vinicios Leite de Campos <vinicios.ldcampos@gmail.com>
2026-06-06 23:21:57 -0300

    feat: implementa Dashboard com métricas, gráficos, eventos e resumo financeiro

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Editor    from "./pages/Editor";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"               element={<Dashboard />} />
+        <Route path="/dashboard"      element={<Dashboard />} />
+        <Route path="/rooms/:id/edit" element={<Editor />}    />
+      </Routes>
+    </BrowserRouter>
+  );
+}
