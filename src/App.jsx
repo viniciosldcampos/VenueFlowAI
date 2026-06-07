96635fc70594ca338acf771eee6bb31ea3a4deea
Vinicios Leite de Campos <vinicios.ldcampos@gmail.com>
2026-06-06 23:48:26 -0300

    feat: implementa página Eventos com tabela, calendário, gráfico por tipo e top salas

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Rooms     from "./pages/Rooms";
 import Editor    from "./pages/Editor";
+import Events from "./pages/Events";
+
 
 export default function App() {
   return (
@@ -10,6 +12,7 @@ export default function App() {
         <Route path="/"               element={<Dashboard />} />
         <Route path="/dashboard"      element={<Dashboard />} />
         <Route path="/rooms"          element={<Rooms />}     />
+        <Route path="/events"         element={<Events />} />
         <Route path="/rooms/:id/edit" element={<Editor />}    />
       </Routes>
     </BrowserRouter>
