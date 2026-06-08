d41bc994b4d3cf3770221cae355acf5c4957230f
Vinicios Leite de Campos <vinicios.ldcampos@gmail.com>
2026-06-07 23:29:28 -0300

    feat: implementa página Calendar com grid mensal, mini calendário e próximos eventos

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Rooms     from "./pages/Rooms";
 import Editor    from "./pages/Editor";
 import Events    from "./pages/Events";
 import Reservations from "./pages/Reservations";
+import CalendarPage from "./pages/Calendar";
 
 
 export default function App() {
@@ -16,6 +17,7 @@ export default function App() {
         <Route path="/rooms/:id/edit" element={<Editor />}    />
         <Route path="/events"         element={<Events />} />
         <Route path="/reservations"   element={<Reservations />} />
+        <Route path="/calendar"       element={<CalendarPage />} />
       </Routes>
     </BrowserRouter>
   );
